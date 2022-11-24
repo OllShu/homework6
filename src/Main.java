3cf60a141aa2cd59d0f7f651d8088956d51143c3
dns <shulgaol@mail.ru>
2022-11-24 13:55:54 +0200

    задание 2

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,13 @@ public class Main {
         for (int i=0; i<=10; i++) {
             System.out.println("Число "+i);
         }
+        System.out.println("____________________________");
+
+        // задание 2
+
+        for (int i=10; i>=0; i--) {
+            System.out.println("Число "+i);
+        }
+        System.out.println("____________________________");
     }
 }
